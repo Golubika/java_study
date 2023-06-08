7b823257131c141e1e07dca70358ed4fef20da3a
Golubika <105418030+Golubika@users.noreply.github.com>
2023-06-08 09:02:55 +0300

    ассоциированные функции

--- sandbox/src/main/java/ru/stqa/javastudy/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/javastudy/sandbox/MyFirstProgram.java
@@ -1,23 +1,21 @@
 package ru.stqa.javastudy.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-         hello("world");
+  public static void main(String[] args) {
+    hello("world");
 
-         double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area (l));
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a, b));
-    }
-    public static void hello (String somebody) {
+    Square s = new Square(5);
 
-        System.out.println("Hello, " + somebody + "!");
-    }
-    public  static double area (double l) {
-        return l*l;
-    }
-    public static double area (double a, double b) {
-        return a*b;
-    }
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+
+
 }
